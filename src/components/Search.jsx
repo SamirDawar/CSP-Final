b4ce1857dca72b022f3bf8848485d8ded51c2588
Samir Dawar <samir@Samirs-MacBook-Pro.local>
2025-02-12 21:18:25 -0500

    search bar, buttons and MUI

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import '../App.css';
+import { Grid2, Button } from "@mui/material";
 
+export default function Search() {
+    const [search, setSearch] = useState('');
 
-export default function Search(){
-    return(
+    function handleSearch(e) {
+        setSearch(e.target.value);
+    }
+
+    return (
         <div className="searchBar">
-            <input placeholder="Search"/>
+            <input 
+                placeholder="Enter your word..."
+                className="SearchBarInput"
+                value={search}
+                onChange={handleSearch}
+            />
+            <Grid2 container spacing={2} justifyContent="center" className="buttonGrid">
+                <Grid2 item>
+                    <Button variant="contained" style={{ margin: '0 10px' }}>
+                        Convert to ASL
+                    </Button>
+                </Grid2>
+                <Grid2 item>
+                    <Button 
+                        variant="contained" 
+                        style={{ margin: '0 10px' }}>
+                        Another Action
+                    </Button>
+                </Grid2>
+            </Grid2>
         </div>
-    )
+    );
 }
